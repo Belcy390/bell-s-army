7bf8fb88cb794d0562278d34afd7484e6c37f29e
Belcy390 <teakookprincess@gmail.com>
2024-11-09 23:32:23 +0530

    added list.html

---
+++ counter.js
@@ -0,0 +1,13 @@
+let counter = 0;
+const counterValue = document.getElementById('counter-value')
+const incrementBtn = document.getElementById('increment-btn')
+const decrementBtn = document.getElementById('decrement-btn')
+
+incrementBtn.addEventListener('click', () => {
+    counter++;
+    counterValue.innerHTML = counter;
+});
+decrementBtn.addEventListener('click',() => {
+    counter--;
+    counterValue.innerHTML = counter;
+});
